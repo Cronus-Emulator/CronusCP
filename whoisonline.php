072e2e1be26ef0bae92e7f967cb881edae45bd54
Matheus Silva de Freitas <matheus.freitas97@gmail.com>
2012-12-15 02:35:16 -0200

    - Adição de mais dois arquivos.

---
+++ whoisonline.php
@@ -0,0 +1,25 @@
+<div id="content-title">Quem está online ?</div>
+<div id="content-html">
+  <table width="100%" border="0" cellpadding="3" id="whoisonline">
+    <thead>
+        <tr>
+          <td>Nome</td>
+          <td>Classe</td>
+          <td>Posição</td>
+        </tr>
+    </thead><?php
+
+	$sql = $mysql->mysqlQuery( 'SELECT `name`,`class`,`last_map` FROM `char`' );
+	$sql = $mysql->mysqlExecute();
+	while( $player = $mysql->mysql_fetchassoc() )
+	{
+		print( "\n\t<tr>\n" );
+			print( "\t\t<td>{$player['name']}</td>\n" );
+			print( "\t\t<td>{$class[$player['class']]}</td>\n" );
+			print( "\t\t<td>{$player['last_map']}</td>\n" );
+		print( "\t</tr>" );
+	}
+
+?>
+  </table>
+</div>
